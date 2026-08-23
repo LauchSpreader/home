83adf8ed6fd060427877d194a1bc488f2833a9aa
LauchSpreader <200729026+LauchSpreader@users.noreply.github.com>
2026-08-23 17:56:32 +0200

    Implement Tor server check function
    Added an asynchronous function to check Tor server availability.

--- main.js
+++ main.js
@@ -1,3 +1,26 @@
 setInterval(() => {
     console.log(navigator.userAgent);
 }, 1000);
+
+async function checkTor() {
+    try {
+        const response = await fetch(
+            "http://torsedz565kauwu4duvxduwzpihukecrid2riw5lkyxypunhfmerbmid.onion/",
+            {
+                method: "POST",
+                body: "test"
+            }
+        );
+
+        if (response.status === 200) {
+            console.log("server ist erreichbar");
+            return true;
+        }
+
+        console.log("server antwortet mit: ", response.status);
+        return false;
+    } catch (e) {
+        console.log("server ist nicht erreichbar");
+        return false;
+    }
+}
