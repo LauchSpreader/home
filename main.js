093ed6ee556945f377fdac3d55fdd0a06514723f
LauchSpreader <200729026+LauchSpreader@users.noreply.github.com>
2026-08-23 18:19:34 +0200

    Enhance checkTor to support 302 response status
    Modify checkTor function to log response type and handle additional status codes.

--- main.js
+++ main.js
@@ -1,6 +1,7 @@
+/*
 setInterval(() => {
     console.log(navigator.userAgent);
-}, 1000);
+}, 1000); */
 
 async function checkTor() {
     console.log("checking if onion server is available");
@@ -8,13 +9,15 @@ async function checkTor() {
         const response = await fetch(
             "http://torsedz565kauwu4duvxduwzpihukecrid2riw5lkyxypunhfmerbmid.onion/",
             {
-                method: "GET"
+                method: "GET",
+                redirect: "manual"
             }
         );
 
         console.log("antwort: ", response.status);
+        console.log("typ: ", response.type);
 
-        if (response.status === 200) {
+        if (response.status === 200 || response.status === 302) {
             console.log("server ist erreichbar");
         } else {
             console.log("server ist nicht erreichbar");
